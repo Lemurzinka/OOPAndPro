9990383632f768a114317e34bff47ac9847234fa
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-07-25 15:28:37 +0300

    Add files via upload

---
+++ oopfirst/Products.java
@@ -0,0 +1,43 @@
+package oopfirst;
+
+public class Products {
+
+	private int price; 
+	private String description; 
+	private double weight;
+	
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public double getWeight() {
+		return weight;
+	}
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	public Products(int price, String description, double weight) {
+		super();
+		this.price = price;
+		this.description = description;
+		this.weight = weight;
+	}
+	public Products() {
+		super();
+	}
+	@Override
+	public String toString() {
+		return "Products [price=" + price + "₴, description=" + description + ", weight=" + weight + " grams]";
+	} 
+	
+	
+	
+}
