d9023f23ded6c87146222d936de8a350c62b34fd
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-07-25 19:15:21 +0300

    Update Main1.java

--- oopfirst/first/Main1.java
+++ oopfirst/first/Main1.java
@@ -4,7 +4,7 @@ public class Main1 {
 
 	public static void main(String[] args) {
 		
-		Product product1 = new Product(80, "Unc.", 110);
+		Product product1 = new Product(80, "Unk.", 110);
 		product1.setDescription("Banana");
 		
 		Product product2 = new Product(70, "Cherry", 55);
