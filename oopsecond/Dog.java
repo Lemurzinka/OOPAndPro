5cf0f441123f2f81a1d22c758dd5e1546b95e69b
Lemurzinka <154348981+Lemurzinka@users.noreply.github.com>
2024-07-30 19:42:28 +0300

    Add files via upload

---
+++ oopsecond/Dog.java
@@ -0,0 +1,50 @@
+package oopsecond;
+
+public class Dog extends Animal {
+private String name;
+
+
+public Dog(String ration, String color, int weight, String name) {
+	super(ration, color, weight);
+	this.name = name;
+}
+
+public Dog() {
+	super();
+	
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+
+@Override
+public String toString() {
+	return "Dog [name=" + name + "]";
+}
+
+
+
+@Override
+public String getVoice() {
+	return "Woof";
+};
+
+@Override
+public void eat () {
+	 System.out.println(name + " is eating");
+};
+
+@Override	
+public void sleep () {
+	 System.out.println(name + " is sleeping");
+};
+
+
+
+}
